3a3a5490f568c65f9761cd233e16d3579cffdc08
Vladimir Shevchenko <vshevchenko@inpromconsult.ru>
2022-10-04 02:41:46 +0300

    refactoring pgp request/response

--- src/main/java/com/malerx/bot/handlers/impl/PgpHandler.java
+++ src/main/java/com/malerx/bot/handlers/impl/PgpHandler.java
@@ -1,11 +1,9 @@
 package com.malerx.bot.handlers.impl;
 
 import com.malerx.bot.data.entity.GpgRecord;
-import com.malerx.bot.data.entity.TGUser;
 import com.malerx.bot.data.enums.Role;
 import com.malerx.bot.data.repository.TGUserRepository;
 import com.malerx.bot.handlers.CommandHandler;
-import io.micronaut.core.util.CollectionUtils;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
@@ -15,9 +13,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import javax.inject.Singleton;
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @Singleton
@@ -37,46 +33,33 @@ public class PgpHandler implements CommandHandler {
         if (update.hasMessage()) {
             log.debug("handle() -> incoming request owner token from {}", update.getMessage().getChatId());
             return tgUserRepository.findByRole(Role.ADMIN)
-                    .thenApply(admin -> {
-                        var message = admin.stream()
-                                .map(TGUser::getGpgPublicKeys)
-                                .reduce((identity, accumulator) -> {
-                                    accumulator.addAll(identity);
-                                    return accumulator;
-                                });
-                        if (message.isPresent()) {
-                            return Optional.of(new SendDocument(
-                                    update.getMessage().getChatId().toString(),
-                                    createInputFile(buildString(message.get()))));
-                        }
-                        return Optional.of(new SendMessage(
-                                update.getMessage().getChatId().toString(),
-                                """
-                                        В локальной базе данных не найдены записи PGP ключей \
-                                        администраторов системы.
-                                        """));
-                    });
-
+                    .thenApply(admins -> admins.stream()
+                            .flatMap(user -> user.getGpgPublicKeys().stream())
+                            .map(GpgRecord::getPgpKey)
+                            .reduce((s1, s2) -> s2.concat("\n\n\n".concat(s1)))
+                            .map(keys -> createDocument(update, keys))
+                            .or(() -> createMessage(update)));
         }
         log.warn("handle() -> incoming request not contain message");
         return CompletableFuture.completedFuture(Optional.empty());
     }
 
-    private String buildString(Set<GpgRecord> records) {
-        StringBuffer sb = new StringBuffer();
-        if (CollectionUtils.isNotEmpty(records))
-            records.forEach(record -> sb.append(record.getPgpKey()).append("\n\n"));
-        return sb.toString();
+    private Object createDocument(Update update, String keys) {
+        return new SendDocument(
+                update.getMessage().getChatId().toString(),
+                new InputFile(new ByteArrayInputStream(
+                        keys.getBytes()),
+                        "keys.txt"));
     }
 
-    private InputFile createInputFile(String keys) {
-        InputFile file = null;
-        try (InputStream is = new ByteArrayInputStream(keys.getBytes())) {
-            file = new InputFile(is, "key.txt");
-        } catch (Exception e) {
-            log.error("handle() -> fail create keys.txt");
-        }
-        return file;
+    private Optional<Object> createMessage(Update update) {
+        return Optional.of(new SendMessage(
+                update.getMessage().getChatId().toString(),
+                """
+                        В локальной базе данных не найдены записи PGP ключей \
+                        администраторов системы.
+                        """
+        ));
     }
 
     @Override
